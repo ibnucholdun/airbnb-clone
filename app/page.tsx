a7929b6184cc69f6d47fd82b7e88ee18bf3f2f42
ibnucholdun <ibnucholdun023@gmail.com>
2024-03-16 19:38:39 +0700

    feat: setup DB + prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import MapFilterItems from "./components/MapFilterItems";
 
 export default function Home() {
   return (
-    <div>
-      <Button>hello works</Button>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
     </div>
   );
 }
